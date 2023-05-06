fca70a7c60d5f94f5c473275761fe73af4d09525
valchri26 <evalda.christina1@gmail.com>
2023-05-06 09:57:49 +0700

    udh bisa nampilin profile screen

--- kivyFile/tipir.py
+++ kivyFile/tipir.py
@@ -1,31 +1,13 @@
 import pyrebase
 import requests
 
-from kivy.core.text import LabelBase
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.uix.label import Label
-from kivy.uix.screenmanager import Screen
+from kivymd.uix.screen import MDScreen
+from kivy.properties import ObjectProperty
 
-# cred = credentials.Certificate('kivyFile/privateKey.json')
-# firebase_admin.initialize_app(cred, {
-#     'databaseURL': 'https://projecttafix-4b39d-default-rtdb.firebaseio.com/'
-# })
-
-# Builder.load_file("home.kv")
-
-# # Get a database client
-# database_url = 'https://projecttafix-4b39d-default-rtdb.firebaseio.com/'
-# ref = db.reference('/', url=database_url)
-
-# config = {
-#   "apiKey": "apiKey",
-#   "authDomain": "projectId.firebaseapp.com",
-#   "databaseURL": "https://databaseName.firebaseio.com",
-#   "storageBucket": "projectId.appspot.com"
-# }
 
 firebaseConfig = {
   "apiKey": "AIzaSyC1cBc7jJrLVZrdSoTS3E-_VoSCwuHmbck",
@@ -58,13 +40,12 @@ class main(MDApp):
         
     def build(self):
         # screen_manager.add_widget(Builder.load_file("pre-splash.kv"))
-        # Builder.load_file('profile_screen.kv')
+        Builder.load_file('profile.kv')
         self.screen_manager.add_widget(Builder.load_file("main.kv"))
         self.screen_manager.add_widget(Builder.load_file("home.kv"))
         self.screen_manager.add_widget(Builder.load_file("login.kv"))
         self.screen_manager.add_widget(Builder.load_file("signup.kv"))
-        self.screen_manager.add_widget(Builder.load_file("profile_screen.kv"))
-        # self.screen_manager.add_widget(profile_screen(name='profile'))
+        self.screen_manager.add_widget(profile_screen(name='profile'))
         return self.screen_manager
     
     def signup(self, email:str, password:str, nim:str, nopol:str, poinkp:str):
@@ -105,7 +86,12 @@ class main(MDApp):
     def sign_out(self):
         pass
 
-class profile_screen(Screen):
+class profile_screen(MDScreen):
+    email_label = ObjectProperty()
+    nim_label = ObjectProperty()
+    nopol_label = ObjectProperty()
+    poinkp_label = ObjectProperty()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.email = ""
@@ -116,21 +102,10 @@ class profile_screen(Screen):
     def on_pre_enter(self):
         print(current_user)
         print(current_user_data)
-        self.email = current_user['email']
-        self.nim = current_user_data['nim']
-        self.nopol = current_user_data['nopol']
-        self.poinkp = current_user_data['poinkp']
-        # # Get a reference to the Mahasiswa node in the database
-        # mahasiswa_ref = db.reference('Mahasiswa')
-        # # Get the user's data from the database
-        # user_data = mahasiswa_ref.child(self.manager.current_user.uid).get()
-        # print(f"current uid: {self.manager.current_user.uid} | user_data: {user_data}")
-        # # Display the user's data on the screen
-        # self.ids.nama_label.text = user_data['Nama']
-        # self.ids.email_label.text = user_data['Email']
-        # self.ids.jurusan_label.text = user_data['Jurusan']
-        # self.ids.nopol_label.text = user_data['Plat_no']
-        # self.ids.poin_label.text = user_data['Poin_kp']  
+        self.email_label.text = current_user['email']
+        self.nim_label.text = current_user_data['nim']
+        self.nopol_label.text = current_user_data['nopol']
+        self.poinkp_label.text = current_user_data['poinkp'] 
             
 if __name__ == '__main__':
     from kivy.config import Config
